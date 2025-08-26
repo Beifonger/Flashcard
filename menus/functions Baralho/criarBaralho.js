452f059e29c376cdb9f349c18e1d5b578abbda94
Beifonger <gabriel.boom1419@gmail.com>
2025-08-26 10:26:36 -0500

    0.3

--- menus/functions Baralho/criarBaralho.js
+++ menus/functions Baralho/criarBaralho.js
@@ -1,3 +1,12 @@
 const perguntar = require("../../utilitarios/pergunta")
 
-function criarBaralho(Baralho,voltar){}
+async function criarBaralho(Baralho,voltar){
+    console.log(`==== Criar baralho ====`)
+    inputBaralho = await perguntar("Nome: ")
+    if(!inputBaralho||!isNaN(inputBaralho)){
+        console.log(`Nome de baralho invalido!`)
+        return criarBaralho
+    }
+    BaralhoAtual = {id:Date.now(), baralho: inputBaralho}
+
+}
